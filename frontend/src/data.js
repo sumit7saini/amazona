a150818268ebcca9b0ec6074e97df264d9faf633
sumitsainiatechnos <sumitsainiatechnos@gmail.com>
2021-07-27 18:32:47 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:'120',
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Addidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:'100',
+            brand:'Addidas',
+            rating:4,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lactose Free Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:'120',
+            brand:'Lactose',
+            rating:4.8,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:'137',
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:'65',
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Addidas Fit Pant',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:'120',
+            brand:'Addidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        }
+    ]
+}
+export default data;
